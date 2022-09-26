dd970159b30b63b750667e6dd65273c749ed0568
gokselkucuksahin <gokselkucuksahin@gmail.com>
2022-09-26 20:22:39 +0300

    Inial Commit.

---
+++ src/components/StopTimer.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { useState } from "react";
+import "../styles/StopTimer.css";
+import { TimeTable } from "./TimeTable";
+
+export type StartStopText = "Start" | "Stop";
+export type TimeResetText = "Time" | "Reset";
+
+function createTimer(mutater: React.Dispatch<React.SetStateAction<Date>>, start: Date, interval = 50) {
+    let startTime = start.getTime();
+    const ref = setInterval(() => {
+        startTime += interval;
+        const date = new Date(startTime);
+        mutater(date);
+    }, interval);
+    return () => {
+        clearInterval(ref);
+    }
+}
+
+function dateToTimeString(date: Date): string {
+    return `${date.getMinutes().toString().padStart(2, "0")} : ${date.getSeconds().toString().padStart(2, "0")} . ${date.getMilliseconds().toString().substring(0, 2).padStart(2, "0")}`;
+}
+
+let stopCounting = () => { };
+let lastTime = new Date(0);
+
+export function StopTimer() {
+    const [timeFace, setTimeFace] = useState<Date>(new Date(0));
+    const [startStopButton, setStartStopButton] = useState<StartStopText>("Start");
+    const [timeResetButton, setTimeResetButton] = useState<TimeResetText>("Time");
+    const [startStopClass, setStartStopClass] = useState("btn mx-3 fixed-btn btn-primary");
+    const [lapTimes, setLapTimes] = useState<string[][]>([]);
+
+    function handleStartStop() {
+        if (startStopButton === "Start") {
+            stopCounting = createTimer(setTimeFace, timeFace, 10);
+        } else {
+            stopCounting()
+        }
+        setStartStopButton(prev => {
+            if (prev === "Start") {
+                setTimeResetButton("Time");
+                setStartStopClass("btn mx-3 fixed-btn btn-danger")
+                return "Stop";
+            }
+            else {
+                setTimeResetButton("Reset");
+                setStartStopClass("btn mx-3 fixed-btn btn-primary");
+                return "Start";
+            }
+        });
+    }
+
+    function handleTime() {
+        if (timeResetButton === "Reset") {
+            setTimeFace(new Date(0));
+            lastTime = new Date(0);
+            setLapTimes([]);
+        } else if (timeResetButton === "Time") {
+            const diff = new Date(timeFace.getTime() - lastTime.getTime());
+            lastTime = timeFace;
+            setLapTimes(prev => {
+                return [...prev, [dateToTimeString(diff), dateToTimeString(timeFace)]];
+            });
+            console.log("tur süresi:", dateToTimeString(diff), "toplam süre:", dateToTimeString(timeFace));
+        }
+    }
+
+    return (
+        <div className="container-fluid blue-tint">
+            <div className='row justify-content-center'>
+                <div className="col-sm-12 col-md-8 px-2">
+                    <h1 className="display-4 mt-4 mb-2" style={{ "fontFamily": "monospace" }}>
+                        {dateToTimeString(timeFace)}
+                    </h1>
+                    <div className="d-flex justify-content-center my-3">
+                        <button
+                            className={startStopClass}
+                            onClick={handleStartStop}>
+                            {startStopButton}
+                        </button>
+                        <button
+                            className='btn btn-light mx-3 fixed-btn'
+                            onClick={handleTime}>
+                            {timeResetButton}
+                        </button>
+                    </div>
+                    <TimeTable laps={lapTimes} />
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dolores, velit amet labore qui corrupti quaerat dicta neque nesciunt consequuntur.</p> */}
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/TimeTable.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface TimeTableProps {
+    laps: string[][];
+}
+
+export function TimeTable({laps}: TimeTableProps) {
+    return (
+        laps.length > 0 ? <div className="mx-5 px-5">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Lap</th>
+                        <th scope="col">Lap Time</th>
+                        <th scope="col">Total Time</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {laps.map((item, index) => {
+                        return (
+                            <tr key={index + 666}>
+                                <th scope="row">{(1 + index).toString().padStart(2, "0")}</th>
+                                <td>{item[0]}</td>
+                                <td>{item[1]}</td>
+                            </tr>)
+                    }).reverse()}
+                    </tbody>
+                </table>
+            </div>
+            : <></>
+    );
+}
